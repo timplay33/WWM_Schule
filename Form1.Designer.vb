b11e51bcd749959a2ea32edfd0a1d1d7fbcff079
theid <theid@DESKTOP-5LC18VA>
2023-04-05 15:16:30 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TXT_Frage = New TextBox()
+        CMD_Start = New Button()
+        CMD_AntwortC = New Button()
+        CMD_AntwortB = New Button()
+        CMD_AntwortD = New Button()
+        LBL_FrageNr = New Label()
+        LBL_Gewinn = New Label()
+        CMD_Weiter = New Button()
+        CMD_50_50_Joker = New Button()
+        CMD_Telefon_Joker = New Button()
+        LBL_Telefon_Joker = New Label()
+        LBL_Zuschauer_Joker = New Label()
+        CMD_Zuschauer_Joker = New Button()
+        LBL_Joker = New Label()
+        CMD_AntwortA = New Button()
+        CMD_Aufhoeren = New Button()
+        SuspendLayout()
+        ' 
+        ' TXT_Frage
+        ' 
+        TXT_Frage.Location = New Point(12, 200)
+        TXT_Frage.Name = "TXT_Frage"
+        TXT_Frage.Size = New Size(776, 23)
+        TXT_Frage.TabIndex = 1
+        TXT_Frage.Visible = False
+        ' 
+        ' CMD_Start
+        ' 
+        CMD_Start.FlatAppearance.BorderSize = 0
+        CMD_Start.Location = New Point(176, 146)
+        CMD_Start.Name = "CMD_Start"
+        CMD_Start.Size = New Size(451, 150)
+        CMD_Start.TabIndex = 2
+        CMD_Start.Text = "Starten"
+        CMD_Start.UseVisualStyleBackColor = True
+        ' 
+        ' CMD_AntwortC
+        ' 
+        CMD_AntwortC.FlatAppearance.BorderSize = 0
+        CMD_AntwortC.FlatStyle = FlatStyle.Flat
+        CMD_AntwortC.Location = New Point(12, 302)
+        CMD_AntwortC.Name = "CMD_AntwortC"
+        CMD_AntwortC.Size = New Size(382, 67)
+        CMD_AntwortC.TabIndex = 7
+        CMD_AntwortC.Text = "C"
+        CMD_AntwortC.UseVisualStyleBackColor = True
+        CMD_AntwortC.Visible = False
+        ' 
+        ' CMD_AntwortB
+        ' 
+        CMD_AntwortB.FlatAppearance.BorderSize = 0
+        CMD_AntwortB.FlatStyle = FlatStyle.Flat
+        CMD_AntwortB.Location = New Point(406, 229)
+        CMD_AntwortB.Name = "CMD_AntwortB"
+        CMD_AntwortB.Size = New Size(382, 67)
+        CMD_AntwortB.TabIndex = 9
+        CMD_AntwortB.Text = "B"
+        CMD_AntwortB.UseVisualStyleBackColor = True
+        CMD_AntwortB.Visible = False
+        ' 
+        ' CMD_AntwortD
+        ' 
+        CMD_AntwortD.FlatAppearance.BorderSize = 0
+        CMD_AntwortD.FlatStyle = FlatStyle.Flat
+        CMD_AntwortD.Location = New Point(406, 302)
+        CMD_AntwortD.Name = "CMD_AntwortD"
+        CMD_AntwortD.Size = New Size(382, 67)
+        CMD_AntwortD.TabIndex = 10
+        CMD_AntwortD.Text = "D"
+        CMD_AntwortD.UseVisualStyleBackColor = True
+        CMD_AntwortD.Visible = False
+        ' 
+        ' LBL_FrageNr
+        ' 
+        LBL_FrageNr.AutoSize = True
+        LBL_FrageNr.Location = New Point(12, 9)
+        LBL_FrageNr.Name = "LBL_FrageNr"
+        LBL_FrageNr.Size = New Size(42, 15)
+        LBL_FrageNr.TabIndex = 11
+        LBL_FrageNr.Text = "Frage: "
+        LBL_FrageNr.Visible = False
+        ' 
+        ' LBL_Gewinn
+        ' 
+        LBL_Gewinn.AutoSize = True
+        LBL_Gewinn.Location = New Point(12, 169)
+        LBL_Gewinn.Name = "LBL_Gewinn"
+        LBL_Gewinn.Size = New Size(13, 15)
+        LBL_Gewinn.TabIndex = 12
+        LBL_Gewinn.Text = "€"
+        LBL_Gewinn.Visible = False
+        ' 
+        ' CMD_Weiter
+        ' 
+        CMD_Weiter.FlatAppearance.BorderSize = 0
+        CMD_Weiter.Location = New Point(621, 389)
+        CMD_Weiter.Name = "CMD_Weiter"
+        CMD_Weiter.Size = New Size(167, 49)
+        CMD_Weiter.TabIndex = 13
+        CMD_Weiter.Text = "Weiter"
+        CMD_Weiter.UseVisualStyleBackColor = True
+        CMD_Weiter.Visible = False
+        ' 
+        ' CMD_50_50_Joker
+        ' 
+        CMD_50_50_Joker.FlatAppearance.BorderSize = 0
+        CMD_50_50_Joker.Location = New Point(713, 31)
+        CMD_50_50_Joker.Name = "CMD_50_50_Joker"
+        CMD_50_50_Joker.Size = New Size(75, 23)
+        CMD_50_50_Joker.TabIndex = 14
+        CMD_50_50_Joker.Text = "50:50"
+        CMD_50_50_Joker.UseVisualStyleBackColor = True
+        CMD_50_50_Joker.Visible = False
+        ' 
+        ' CMD_Telefon_Joker
+        ' 
+        CMD_Telefon_Joker.FlatAppearance.BorderSize = 0
+        CMD_Telefon_Joker.Location = New Point(713, 60)
+        CMD_Telefon_Joker.Name = "CMD_Telefon_Joker"
+        CMD_Telefon_Joker.Size = New Size(75, 23)
+        CMD_Telefon_Joker.TabIndex = 15
+        CMD_Telefon_Joker.Text = "Telefon"
+        CMD_Telefon_Joker.UseVisualStyleBackColor = True
+        CMD_Telefon_Joker.Visible = False
+        ' 
+        ' LBL_Telefon_Joker
+        ' 
+        LBL_Telefon_Joker.AutoSize = True
+        LBL_Telefon_Joker.Location = New Point(688, 64)
+        LBL_Telefon_Joker.Name = "LBL_Telefon_Joker"
+        LBL_Telefon_Joker.Size = New Size(10, 15)
+        LBL_Telefon_Joker.TabIndex = 16
+        LBL_Telefon_Joker.Text = " "
+        ' 
+        ' LBL_Zuschauer_Joker
+        ' 
+        LBL_Zuschauer_Joker.AutoSize = True
+        LBL_Zuschauer_Joker.Location = New Point(688, 93)
+        LBL_Zuschauer_Joker.Name = "LBL_Zuschauer_Joker"
+        LBL_Zuschauer_Joker.Size = New Size(10, 15)
+        LBL_Zuschauer_Joker.TabIndex = 18
+        LBL_Zuschauer_Joker.Text = " "
+        ' 
+        ' CMD_Zuschauer_Joker
+        ' 
+        CMD_Zuschauer_Joker.FlatAppearance.BorderSize = 0
+        CMD_Zuschauer_Joker.Location = New Point(713, 89)
+        CMD_Zuschauer_Joker.Name = "CMD_Zuschauer_Joker"
+        CMD_Zuschauer_Joker.Size = New Size(75, 23)
+        CMD_Zuschauer_Joker.TabIndex = 17
+        CMD_Zuschauer_Joker.Text = "Zuschauer"
+        CMD_Zuschauer_Joker.UseVisualStyleBackColor = True
+        CMD_Zuschauer_Joker.Visible = False
+        ' 
+        ' LBL_Joker
+        ' 
+        LBL_Joker.AutoSize = True
+        LBL_Joker.Location = New Point(729, 13)
+        LBL_Joker.Name = "LBL_Joker"
+        LBL_Joker.Size = New Size(34, 15)
+        LBL_Joker.TabIndex = 19
+        LBL_Joker.Text = "Joker"
+        LBL_Joker.Visible = False
+        ' 
+        ' CMD_AntwortA
+        ' 
+        CMD_AntwortA.FlatAppearance.BorderSize = 0
+        CMD_AntwortA.FlatStyle = FlatStyle.Flat
+        CMD_AntwortA.ForeColor = SystemColors.ControlText
+        CMD_AntwortA.Location = New Point(12, 229)
+        CMD_AntwortA.Name = "CMD_AntwortA"
+        CMD_AntwortA.Size = New Size(382, 67)
+        CMD_AntwortA.TabIndex = 21
+        CMD_AntwortA.Text = "A"
+        CMD_AntwortA.UseVisualStyleBackColor = True
+        CMD_AntwortA.Visible = False
+        ' 
+        ' CMD_Aufhoeren
+        ' 
+        CMD_Aufhoeren.FlatAppearance.BorderSize = 0
+        CMD_Aufhoeren.Location = New Point(12, 389)
+        CMD_Aufhoeren.Name = "CMD_Aufhoeren"
+        CMD_Aufhoeren.Size = New Size(167, 49)
+        CMD_Aufhoeren.TabIndex = 22
+        CMD_Aufhoeren.Text = "Aufhören"
+        CMD_Aufhoeren.UseVisualStyleBackColor = True
+        CMD_Aufhoeren.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DodgerBlue
+        ClientSize = New Size(800, 450)
+        Controls.Add(CMD_Aufhoeren)
+        Controls.Add(CMD_AntwortA)
+        Controls.Add(LBL_Joker)
+        Controls.Add(LBL_Zuschauer_Joker)
+        Controls.Add(CMD_Zuschauer_Joker)
+        Controls.Add(LBL_Telefon_Joker)
+        Controls.Add(CMD_Telefon_Joker)
+        Controls.Add(CMD_50_50_Joker)
+        Controls.Add(CMD_Weiter)
+        Controls.Add(LBL_Gewinn)
+        Controls.Add(LBL_FrageNr)
+        Controls.Add(CMD_AntwortD)
+        Controls.Add(CMD_AntwortB)
+        Controls.Add(CMD_AntwortC)
+        Controls.Add(CMD_Start)
+        Controls.Add(TXT_Frage)
+        Name = "Form1"
+        Text = "Wer Wird Milionär"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TXT_Frage As TextBox
+    Friend WithEvents CMD_Start As Button
+    Friend WithEvents CMD_AntwortC As Button
+    Friend WithEvents CMD_AntwortB As Button
+    Friend WithEvents CMD_AntwortD As Button
+    Friend WithEvents LBL_FrageNr As Label
+    Friend WithEvents LBL_Gewinn As Label
+    Friend WithEvents CMD_Weiter As Button
+    Friend WithEvents CMD_50_50_Joker As Button
+    Friend WithEvents CMD_Telefon_Joker As Button
+    Friend WithEvents LBL_Telefon_Joker As Label
+    Friend WithEvents LBL_Zuschauer_Joker As Label
+    Friend WithEvents CMD_Zuschauer_Joker As Button
+    Friend WithEvents LBL_Joker As Label
+    Friend WithEvents CMD_AntwortA As Button
+    Friend WithEvents CMD_Aufhoeren As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,382 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+
+    Public Fragen(15, 7) As String
+    Public FrageNr As Double
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FrageNr = 1
+        'Fragen
+        Fragen(1, 1) = "Zu vorgerückter Stunde beeilten sich die Wanderer, damit sie es noch vor Einbruch der Dunkelheit bis zum nächsten ...?"
+        Fragen(2, 1) = "Wühlt der Hund mit seinem Gesicht im Schnee, ist er danach sozusagen ...?"
+        Fragen(3, 1) = "Wem während des Einkaufs der Drahtesel gestohlen wird, der steht vor dem Laden erst mal völlig ...?"
+        Fragen(4, 1) = "Wovon findet man die Version mit 'W' am Anfang beim Einkaufen oft im selben Regal?"
+        Fragen(5, 1) = "Welche Präposition ist auch für Weinkenner nicht weit hergeholt?"
+        Fragen(6, 1) = "Was stieg mit Jahresbeginn 2023 auf 250 Euro im Monat?"
+        Fragen(7, 1) = "Wer gehört zu den Kolleginnen von Merle Frohms, Giulia Gwinn, Lina Magull, Sara Däbritz und Marina Hegering?"
+        Fragen(8, 1) = "Wer ist ständiger Begleiter von Wednesday aus der Addams Family, der derzeit wohl angesagtesten Serien-Teenagerin?"
+        Fragen(9, 1) = "Wie nennt der Engländer seine Vermieterin?"
+        Fragen(10, 1) = "Abstimmungskrimi in Washington: Wer brauchte 2023 ganze 15 Wahlgänge, um Sprecher des Repräsentantenhauses zu werden??"
+        Fragen(11, 1) = "Welche Videospielreihe wird nach fast 30 Jahren ab 2023 nicht mehr von EA SPORTS unter diesem Namen weitergeführt?"
+        Fragen(12, 1) = "Im Medizinerlatein heißt ...?"
+        Fragen(13, 1) = "Auf wie viele heutige Staaten verteilen sich die Standorte der sieben Weltwunder der Antike?"
+        Fragen(14, 1) = "Bibi Blocksberg heißt eigentlich ...?"
+        Fragen(15, 1) = "Wo fand 2016 zum bislang letzten Mal ein Formel-1-Rennen um den Großen Preis von Europa statt?     "
+
+        'Antworten
+        Fragen(1, 2) = "Klein stätte"
+        Fragen(1, 3) = "Siedlunngen"
+        Fragen(1, 4) = "Ort schafften"
+        Fragen(1, 5) = "Kuh dörfer"
+
+        Fragen(2, 2) = "erbarmungslos"
+        Fragen(2, 3) = "abgebrüht"
+        Fragen(2, 4) = "unnachgiebig"
+        Fragen(2, 5) = "kaltschnäuzig"
+
+        Fragen(3, 2) = "schutslos"
+        Fragen(3, 3) = "radlos"
+        Fragen(3, 4) = "plahnlos"
+        Fragen(3, 5) = "hillflos"
+
+        Fragen(4, 2) = "Vollmilchschokolade"
+        Fragen(4, 3) = "Vollkornbrot"
+        Fragen(4, 4) = "Vollwaschmittel"
+        Fragen(4, 5) = "Vollwertmüsli"
+
+        Fragen(5, 2) = "mosel"
+        Fragen(5, 3) = "ahr"
+        Fragen(5, 4) = "nahe"
+        Fragen(5, 5) = "rhein"
+
+        Fragen(6, 2) = "Rundfunkbeitrag"
+        Fragen(6, 3) = "Hundesteuer"
+        Fragen(6, 4) = "Kindergeld"
+        Fragen(6, 5) = "m²-Mietpreis in München"
+
+        Fragen(7, 2) = "Annalena Baerbock"
+        Fragen(7, 3) = "Alexandra Popp"
+        Fragen(7, 4) = "Jil Sander"
+        Fragen(7, 5) = "Cornelia Poletto"
+
+        Fragen(8, 2) = "Laufendes Näschen"
+        Fragen(8, 3) = "Eiskaltes Händchen"
+        Fragen(8, 4) = "Zuckendes Äuglein"
+        Fragen(8, 5) = "Stinkendes Füßchen"
+
+        Fragen(9, 2) = "landlady"
+        Fragen(9, 3) = "villagequeen"
+        Fragen(9, 4) = "citymadam"
+        Fragen(9, 5) = "townmiss"
+
+        Fragen(10, 2) = "Nixon"
+        Fragen(10, 3) = "Eisenhower"
+        Fragen(10, 4) = "McCarthy"
+        Fragen(10, 5) = "Hoover"
+
+        Fragen(11, 2) = "Grand Theft Auto"
+        Fragen(11, 3) = "Die Sims"
+        Fragen(11, 4) = "FIFA"
+        Fragen(11, 5) = "Super Mario Kart"
+
+        Fragen(12, 2) = "die Aschelhöhle Axilla"
+        Fragen(12, 3) = "die Kniekehle Knillia"
+        Fragen(12, 4) = "die Ohrmuschel Orilla"
+        Fragen(12, 5) = "der Bauchnabel Nabilla"
+
+        Fragen(13, 2) = "zwei"
+        Fragen(13, 3) = "drei"
+        Fragen(13, 4) = "vier"
+        Fragen(13, 5) = "fünf"
+
+        Fragen(14, 2) = "Barbara"
+        Fragen(14, 3) = "Brigitte"
+        Fragen(14, 4) = "Bianca"
+        Fragen(14, 5) = "Beatrix"
+
+        Fragen(15, 2) = "Baku"
+        Fragen(15, 3) = "Valencia"
+        Fragen(15, 4) = "Nürburgring"
+        Fragen(15, 5) = "Spa-Francorchamps"
+        'Richtige Antwort
+        Fragen(1, 6) = "C"
+        Fragen(2, 6) = "D"
+        Fragen(3, 6) = "B"
+        Fragen(4, 6) = "C"
+        Fragen(5, 6) = "C"
+        Fragen(6, 6) = "C"
+        Fragen(7, 6) = "B"
+        Fragen(8, 6) = "B"
+        Fragen(9, 6) = "A"
+        Fragen(10, 6) = "C"
+        Fragen(11, 6) = "C"
+        Fragen(12, 6) = "A"
+        Fragen(13, 6) = "C"
+        Fragen(14, 6) = "B"
+        Fragen(15, 6) = "A"
+
+        'value
+        Fragen(1, 7) = "50"
+        Fragen(2, 7) = "100"
+        Fragen(3, 7) = "200"
+        Fragen(4, 7) = "300"
+        Fragen(5, 7) = "500"
+        Fragen(6, 7) = "1000"
+        Fragen(7, 7) = "2000"
+        Fragen(8, 7) = "4000"
+        Fragen(9, 7) = "8000"
+        Fragen(10, 7) = "16000"
+        Fragen(11, 7) = "32000"
+        Fragen(12, 7) = "64000"
+        Fragen(13, 7) = "125000"
+        Fragen(14, 7) = "500000"
+        Fragen(15, 7) = "1000000"
+
+    End Sub
+
+    Public Function TryAnswer(ByVal answer As String) As Boolean
+        CMD_Aufhoeren.Enabled = False
+        If Fragen(FrageNr, 6) = answer Then
+            CorrectAnswer()
+            Return True
+        End If
+
+        WrongAnswer()
+        Return False
+    End Function
+
+    Public Sub WrongAnswer()
+        CMD_AntwortA.Enabled = False
+        CMD_AntwortB.Enabled = False
+        CMD_AntwortC.Enabled = False
+        CMD_AntwortD.Enabled = False
+        FrageNr = 1
+        EndGame(False)
+        CMD_Weiter.Text = "Versuche es erneut"
+
+    End Sub
+    Public Sub CorrectAnswer()
+        CMD_AntwortA.Enabled = False
+        CMD_AntwortB.Enabled = False
+        CMD_AntwortC.Enabled = False
+        CMD_AntwortD.Enabled = False
+
+        FrageNr = FrageNr + 1
+        If FrageNr >= 16 Then
+            EndGame(True)
+        Else
+            CMD_Weiter.Visible = True
+        End If
+    End Sub
+
+    Public Sub EndGame(ByVal win As Boolean)
+
+        CMD_Weiter.Visible = True
+
+        LBL_Telefon_Joker.Text = ""
+        LBL_Zuschauer_Joker.Text = ""
+
+        If win Then
+            TXT_Frage.Text = "Du hast gewonnen!"
+            FrageNr = 1
+            CMD_Weiter.Text = "Nochmal Spielen"
+        Else
+            TXT_Frage.Text = "Du hast verloren!"
+        End If
+    End Sub
+
+    Public Sub LoadData()
+
+        CMD_AntwortA.Enabled = True
+        CMD_AntwortB.Enabled = True
+        CMD_AntwortC.Enabled = True
+        CMD_AntwortD.Enabled = True
+
+        CMD_AntwortA.Visible = True
+        CMD_AntwortB.Visible = True
+        CMD_AntwortC.Visible = True
+        CMD_AntwortD.Visible = True
+
+        CMD_50_50_Joker.Visible = True
+        CMD_Telefon_Joker.Visible = True
+        CMD_Zuschauer_Joker.Visible = True
+        LBL_Joker.Visible = True
+
+        LBL_Telefon_Joker.Text = ""
+        LBL_Zuschauer_Joker.Text = ""
+
+        CMD_Aufhoeren.Visible = True
+        CMD_Start.Visible = False
+        TXT_Frage.Visible = True
+        LBL_FrageNr.Visible = True
+        LBL_Gewinn.Visible = True
+
+        Dim AntwortA As String
+        Dim AntwortB As String
+        Dim AntwortC As String
+        Dim AntwortD As String
+
+        TXT_Frage.Text = Fragen(FrageNr, 1)
+        AntwortA = "A: " + Fragen(FrageNr, 2)
+        AntwortB = "B: " + Fragen(FrageNr, 3)
+        AntwortC = "C: " + Fragen(FrageNr, 4)
+        AntwortD = "D: " + Fragen(FrageNr, 5)
+
+        CMD_AntwortA.Text = AntwortA
+        CMD_AntwortB.Text = AntwortB
+        CMD_AntwortC.Text = AntwortC
+        CMD_AntwortD.Text = AntwortD
+
+        CMD_AntwortA.BackColor = Color.DodgerBlue
+        CMD_AntwortB.BackColor = Color.DodgerBlue
+        CMD_AntwortC.BackColor = Color.DodgerBlue
+        CMD_AntwortD.BackColor = Color.DodgerBlue
+
+        CMD_Weiter.Text = "Weiter"
+        LBL_FrageNr.Text = "Frage: " + Str(FrageNr)
+        LBL_Gewinn.Text = Fragen(FrageNr, 7) + " €"
+        CMD_Aufhoeren.Enabled = True
+
+    End Sub
+
+    Private Sub CMD_AntwortA_Click(sender As Object, e As EventArgs) Handles CMD_AntwortA.Click
+        If TryAnswer("A") Then
+            CMD_AntwortA.BackColor = Color.Green
+        Else
+            CMD_AntwortA.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub CMD_AntwortB_Click(sender As Object, e As EventArgs) Handles CMD_AntwortB.Click
+        If TryAnswer("B") Then
+            CMD_AntwortB.BackColor = Color.Green
+        Else
+            CMD_AntwortB.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub CMD_AntwortC_Click(sender As Object, e As EventArgs) Handles CMD_AntwortC.Click
+        If TryAnswer("C") Then
+            CMD_AntwortC.BackColor = Color.Green
+        Else
+            CMD_AntwortC.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub CMD_AntwortD_Click(sender As Object, e As EventArgs) Handles CMD_AntwortD.Click
+        If TryAnswer("D") Then
+            CMD_AntwortD.BackColor = Color.Green
+        Else
+            CMD_AntwortD.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub CMD_Start_Click(sender As Object, e As EventArgs) Handles CMD_Start.Click
+        LoadData()
+    End Sub
+
+    Private Sub CMD_Weiter_Click(sender As Object, e As EventArgs) Handles CMD_Weiter.Click
+        If CMD_Weiter.Text = "Weiter" Then
+            LoadData()
+        Else
+            FrageNr = 1
+            LoadData()
+            CMD_50_50_Joker.Enabled = True
+            CMD_Telefon_Joker.Enabled = True
+            CMD_Zuschauer_Joker.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub CMD_50_50_Joker_Click(sender As Object, e As EventArgs) Handles CMD_50_50_Joker.Click
+        CMD_50_50_Joker.Enabled = False
+        Randomize()
+        Dim wrongAnswer1 As String = "ABCD"(Int(Rnd() * 4))
+        Dim wrongAnswer2 As String = "ABCD"(Int(Rnd() * 4))
+
+        While wrongAnswer1 = Fragen(FrageNr, 6)
+            wrongAnswer1 = "ABCD"(Int(Rnd() * 4))
+        End While
+
+        While wrongAnswer2 = Fragen(FrageNr, 6) Or wrongAnswer2 = wrongAnswer1
+            wrongAnswer2 = "ABCD"(Int(Rnd() * 4))
+        End While
+
+        If wrongAnswer1 = "A" Then
+            CMD_AntwortA.Visible = False
+        ElseIf wrongAnswer1 = "B" Then
+            CMD_AntwortB.Visible = False
+        ElseIf wrongAnswer1 = "C" Then
+            CMD_AntwortC.Visible = False
+        ElseIf wrongAnswer1 = "D" Then
+            CMD_AntwortD.Visible = False
+        End If
+
+        If wrongAnswer2 = "A" Then
+            CMD_AntwortA.Visible = False
+        ElseIf wrongAnswer2 = "B" Then
+            CMD_AntwortB.Visible = False
+        ElseIf wrongAnswer2 = "C" Then
+            CMD_AntwortC.Visible = False
+        ElseIf wrongAnswer2 = "D" Then
+            CMD_AntwortD.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub CMD_Telefon_Joker_Click(sender As Object, e As EventArgs) Handles CMD_Telefon_Joker.Click
+        Dim Answer As String
+        CMD_Telefon_Joker.Enabled = False
+        If Rnd() < 0.9 Then
+            Answer = Fragen(FrageNr, 6)
+        Else
+            Answer = "ABCD"(Int(Rnd() * 4))
+            While Answer = Fragen(FrageNr, 6)
+                Answer = "ABCD"(Int(Rnd() * 4))
+            End While
+        End If
+
+        LBL_Telefon_Joker.Text = Answer
+
+    End Sub
+
+    Private Sub CMD_Zuschauer_Joker_Click(sender As Object, e As EventArgs) Handles CMD_Zuschauer_Joker.Click
+        Dim Answer As String
+        CMD_Zuschauer_Joker.Enabled = False
+        If Rnd() < 0.4 Then
+            Answer = Fragen(FrageNr, 6)
+        Else
+            Answer = "ABCD"(Int(Rnd() * 4))
+            While Answer = Fragen(FrageNr, 6)
+                Answer = "ABCD"(Int(Rnd() * 4))
+            End While
+        End If
+
+        LBL_Zuschauer_Joker.Text = Answer
+    End Sub
+
+    Private Sub CMD_Aufhoeren_Click(sender As Object, e As EventArgs) Handles CMD_Aufhoeren.Click
+
+        CMD_AntwortA.Visible = False
+        CMD_AntwortB.Visible = False
+        CMD_AntwortC.Visible = False
+        CMD_AntwortD.Visible = False
+        CMD_50_50_Joker.Visible = False
+        CMD_Telefon_Joker.Visible = False
+        CMD_Zuschauer_Joker.Visible = False
+        LBL_Joker.Visible = False
+        LBL_Gewinn.Visible = False
+
+        If FrageNr > 1 Then
+            TXT_Frage.Text = "Du hast Aufgehört und " + Fragen(FrageNr - 1, 7) + " € gewonnen"
+        Else
+            TXT_Frage.Text = "Du hast Aufgehört und kein Geld gewonnen"
+        End If
+
+        CMD_Weiter.Visible = True
+        CMD_Weiter.Text = "Nochmal Spielen"
+
+
+    End Sub
+End Class
